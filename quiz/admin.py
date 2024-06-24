5299dcae19a935c04a3e627a8fcc1108719d51b8
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-06-24 07:38:13 +0100

    register category

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from quiz.models import Quiz, User
+from quiz.models import Quiz, User, Category
 
 
 # Register your models here.
@@ -8,6 +8,10 @@ from quiz.models import Quiz, User
 class QuizAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'date_created')
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+
+
+admin.site.register(Category)
